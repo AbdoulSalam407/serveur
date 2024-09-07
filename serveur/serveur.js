b8d2daa14ac9889505ea1731fc0af78a691c7369
AbdoulSalam407 <abdoulsalam742@gmail.com>
2024-09-07 22:00:45 +0000

    init

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -8,6 +8,18 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const app = express();
 const PORT = 3000;
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,multipart/form-data"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
